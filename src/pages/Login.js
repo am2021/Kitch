0d86e699191576b4585238f2b08207153014763a
am2021 <58558673+am2021@users.noreply.github.com>
2020-07-16 17:34:05 -0400

    added login / sign-up forms (front-end only)

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Form, Button } from 'react-bootstrap'
 
 const Login = () => {
     return(
         <div>
-            <p>Login</p>
+            <Form>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+
+                <div>
+                    <Button variant="primary" type="submit">
+                        Login
+                    </Button>{' '}
+
+                    <Button variant="secondary" href="/signup">
+                        Sign-up
+                    </Button>
+                </div>
+            </Form>
         </div>
     )
 }
