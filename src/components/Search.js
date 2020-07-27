0251d0536f02a67b4665fa3d0a209be96fc77c60
am2021 <58558673+am2021@users.noreply.github.com>
2020-07-27 18:54:47 -0400

    adding api calls, having trouble hiding key. will change later

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { InputGroup, FormControl, Button } from 'react-bootstrap'
 
-const Search = ({ text, button }) => {
+const Search = ({ text, button, handleFilterChange, handleSubmit }) => {
     return(
         <div>
             <InputGroup>
-                <FormControl placeholder={text} />
+                <FormControl placeholder={text} onKeyUp={handleFilterChange}/>
                 
                 <InputGroup.Append>
-                    <Button variant="outline-secondary">{button}</Button>
+                    <Button variant="outline-secondary" onClick={handleSubmit}>{button}</Button>
                 </InputGroup.Append>    
             </InputGroup>
         </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,47 @@
-import React from 'react';
-import { Container, Row, Col, Image, Form } from 'react-bootstrap'
+import React, { useState } from 'react';
+import { Container, Row, Col, Image } from 'react-bootstrap'
 import ingredients from "../img/ingredients.png"
 import cooking from "../img/cooking.png"
 import Message from '../components/Message'
 import Search from '../components/Search'
 
 const Home = () => {
+    const axios = require("axios");
     // const [ recipes, setRecipes ] = useState([])
-    // const [ filter, setFilter ] = useState('')
+    const [ recipeFilter, setRecipeFilter ] = useState('')
+
+    const handleRecipeFilter = (event) => {
+        setRecipeFilter(event.target.value)
+    }
+
+    const handleRecipeSubmit = (event) => {
+        event.preventDefault()
+
+        if(recipeFilter.length < 1) {
+            console.log("nothing")
+        } else {
+            axios({
+                "method":"GET",
+                "url":"https://edamam-recipe-search.p.rapidapi.com/search",
+                "headers":{
+                "content-type":"application/octet-stream",
+                "x-rapidapi-host":"edamam-recipe-search.p.rapidapi.com",
+                "x-rapidapi-key":"903c62b802msh6ebe5f0e5deed15p17b2b6jsn8271aca746af",
+                "useQueryString":true
+                },"params":{
+                "q":{recipeFilter}
+                }
+                })
+                .then((response)=>{
+                  console.log(response)
+                })
+                .catch((error)=>{
+                  console.log(error)
+                })
+        }
+    }
     
+
 
     return(
         <div>
@@ -26,10 +59,10 @@ const Home = () => {
             <Container>
                 <Row className="justify-content-md-center">
                     <Col id="column">
-                        <Search text="Search for recipes" button="go"/>
+                        <Search text="Search for recipes" button="go" handleFilterChange={handleRecipeFilter} handleSubmit={handleRecipeSubmit}/>
                     </Col>
                     <Col id="column">
-                        <Search text="Search for ingredients" button="go"/>
+                        {/* <Search text="Search for ingredients" button="go"/> */}
                     </Col>
                 </Row>
             </Container>
